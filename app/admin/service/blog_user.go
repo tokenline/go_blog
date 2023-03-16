7f8a13ea022e674e2abfa85ac1dddfdafe3dd98e
Token Token Token <donghui.wang@ucdconnect.ie>
2023-03-16 19:47:36 +0000

    尝试重写update

--- app/admin/service/blog_user.go
+++ app/admin/service/blog_user.go
@@ -35,16 +35,48 @@ func (b *BlogUserService) Update(updateDto dto.BlogUserUpdateDto) error {
 
 	var user = &model.BlogUser{}
 
-	err := global.DB.Model(&user).Where("id = ?", updateDto.ID).Updates(map[string]interface{}{
+	global.DB.Model(&user).Where("ID = ?", updateDto.ID).Find(&user)
+
+	// 判断updateDto中空的选项，并填入原有user中的信息
+
+	// 感觉写一堆if 好蠢
+
+	if updateDto.Nickname == "" {
+		updateDto.Nickname = user.Nickname
+	}
+
+	if updateDto.Mail == "" {
+		updateDto.Mail = user.Mail
+	}
+
+	if updateDto.Phone == "" {
+		updateDto.Phone = user.Phone
+	}
+
+	// 性别和状态都遇到了一样的问题，这里如何表示nil?
+	// if updateDto.Gender == nil {
+	// 	updateDto.Gender = user.Gender
+	// }
+
+	// if updateDto.Status == nil {
+	// 	updateDto.Status = user.Status
+	// }
+
+	if updateDto.Remark == "" {
+		updateDto.Remark = user.Remark
+	}
+
+	// 之前的ID怎么是小写
+	global.DB.Model(&user).Where("id = ?", updateDto.ID).Updates(map[string]interface{}{
 		"nickname": updateDto.Nickname,
 		"mail":     updateDto.Mail,
 		"phone":    updateDto.Phone,
 		"gender":   updateDto.Gender,
 		"status":   updateDto.Status,
 		"remark":   updateDto.Remark,
-	}).Error
+	})
 
-	return err
+	return nil
 }
 
 // 删除
@@ -61,5 +93,13 @@ func (b *BlogUserService) List() error {
 	var users []model.BlogUser
 	err := global.DB.Model(&users).Where("status = ?", 0).Find(&users).Error
 	fmt.Println(users)
+	return err
+}
+
+// QueryByID
+func (b *BlogUserService) Query(ID int) error {
+	var user []model.BlogUser
+	err := global.DB.Model(&user).Where("ID = ?", ID).Find(&user).Error
+
 	return err
 }
