1f7458b468c639eb7646844b107fde727831e81c
chenkunwei <github.com>
2023-03-19 14:35:08 +0800

    引入Swag

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"go-blog/common/global"
 	"go-blog/common/initialize"
+	"go-blog/docs"
 	"go-blog/router"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func main() {
@@ -16,5 +23,20 @@ func main() {
 	//初始化路由
 	var router = router.InitRouters()
 
-	router.Run()
+	InitSwagger(router)
+
+	// router.Run()
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+	fmt.Printf("Swagger文档：http://127.0.0.1%s/swagger/index.html", server.Addr)
+	server.ListenAndServe()
+}
+
+func InitSwagger(router *gin.Engine) {
+	docs.SwaggerInfo.Title = "go-blog"
+	docs.SwaggerInfo.Description = "Swagger Admin API"
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 }
