a1920af6a985ddea305e99c2ba143bee96f35606
Token Token Token <donghui.wang@ucdconnect.ie>
2023-03-21 23:00:12 +0000

    更新路由 增加文章和文章目录的路由

--- router/router_admin.go
+++ router/router_admin.go
@@ -18,6 +18,28 @@ func InitAdminRouters(routerGroup *gin.RouterGroup) {
 		blogUser.GET("list", blogUserApi.List)
 	}
 
+	var blogArticleApi = api.BlogArticleApi{}
+	blogArticle := routerGroup.Group("/blog/article")
+	{
+		blogArticle.GET("query", blogArticleApi.Query)
+		blogArticle.POST("add", blogArticleApi.Add)
+		blogArticle.POST("update", blogArticleApi.Update)
+		blogArticle.POST("delete", blogArticleApi.Delete)
+		//blogArticle.GET("detail", blogArticleApi.Query) 这里需要吗？
+		blogArticle.GET("list", blogArticleApi.List)
+	}
+
+	var blogCategoryApi = api.BlogCategoryApi{}
+	blogCategory := routerGroup.Group("/blog/category")
+	{
+		blogCategory.GET("query", blogCategoryApi.Query)
+		blogCategory.POST("add", blogCategoryApi.Add)
+		blogCategory.POST("update", blogCategoryApi.Update)
+		blogCategory.POST("delete", blogCategoryApi.Delete)
+		//blogCategory.GET("detail", blogCategoryApi.Query) 这里需要吗？
+		blogCategory.GET("list", blogCategoryApi.List)
+	}
+
 	var blogAuthApi = api.BlogAuthApi{}
 	blogAuth := routerGroup.Group("/blog/auth")
 	{
